266a383fd2f23dfa413775476367ad4860e97326
Josué López <jomalolep@gmail.com>
2021-06-27 19:53:29 -0500

    Express, Node, Cowsay

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cowsay = require("cowsay");
+
+app.get('/', (req, res) => {
+    res.set('Content-Type', 'application/json');
+    res.send(cowsay.think({
+        text : "Modulo Nivelador",
+        e : "OO",
+        T : "U "
+    }));
+});
+
+app.listen(3001, () => {
+    console.log("EL SERVIDOR PUERTO 3001");
+})
+
